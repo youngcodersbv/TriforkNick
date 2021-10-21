73eaefbd1fb19126bcc6bfe1c27d3b8e709a2839
Nick Hiert <nickhiert@hotmail.com>
2021-10-21 15:35:07 +0200

    Thymeleaf Category @lh:8080

--- src/main/java/com/Nick/DishProject/controller/th/ThCategoryController.java
+++ src/main/java/com/Nick/DishProject/controller/th/ThCategoryController.java
@@ -6,11 +6,18 @@ import com.Nick.DishProject.model.Category;
 import com.Nick.DishProject.service.CategoryService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
+import java.util.Comparator;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 @Controller
 public class ThCategoryController {
@@ -25,8 +32,15 @@ public class ThCategoryController {
 
     @GetMapping("/categories")
     public String categoryIndex(Model model) {
-        Iterable iter = categoryService.findAllCategories();
-        model.addAttribute("categories",iter);
+        List<Category> categories = categoryService.findAllCategories();
+        List<Category> sortedList = categories.stream()
+                .sorted((Comparator.comparing(Category::getId)))
+                .collect(Collectors.toList());
+
+        model.addAttribute("categories",(Iterable<Category>) sortedList);
+        if(model.getAttribute("category") == null) {
+            model.addAttribute("category", new Category());
+        }
         return "categories";
     }
 
@@ -37,28 +51,34 @@ public class ThCategoryController {
         return "redirect:/category";
     }
 
-
-
     @GetMapping("/category")
     public String categoryOverview() {
         return "/category";
     }
 
     @PostMapping("/categories")
-    public String postNewCategory(@ModelAttribute Category category) {
-        if(category.getId() != null) {
-            Category pCategory = categoryService.findCategoryById(category.getId());
-            pCategory.setType(category.getType());
-            categoryController.updateCategory(pCategory);
-            return "redirect:/categories";
+    public String postNewCategory(@Valid @ModelAttribute Category category, BindingResult result, Model model) {
+        if(result.hasErrors()) {
+            model.addAttribute("category",category);
+            return categoryIndex(model);
         }
         categoryController.addCategory(category);
         return "redirect:/categories";
     }
 
     @PutMapping("/categories")
-    public String updateCategory(@ModelAttribute Category category) {
+    public String updateCategory(@Valid @ModelAttribute Category category, BindingResult result, Model model) {
+        if(result.hasErrors()) {
+            model.addAttribute("category",category);
+            return categoryOverview();
+        }
         categoryController.updateCategory(category);
+        return "redirect:/categories";
+    }
+
+    @DeleteMapping("/categories")
+    public String deleteCategory(@ModelAttribute Category category) {
+        categoryController.deleteCategoryById(category.getId());
         return "redirect:/categories";
     }
 }
